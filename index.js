6b52e9d1e95da3ddab4548705a6a097d0c8e7771
Samir Khadka <47472827+samirkhadka998@users.noreply.github.com>
2023-10-17 21:30:52 +0545

    Calculate winning function complete

--- index.js
+++ index.js
@@ -45,20 +45,53 @@ function addItem(e) {
             input = new tictac(e.target.className, e.target.textContent)
 
         }
+
     }
     inputs.push(input)
+    calculateWinner(input);
 
     display();
 }
 
-function calculateWinner() {
+function calculateWinner(input) {
 
+    let winningCombos = [
+        [1,2,3], 
+        [4,5,6],
+        [7,8,9],
+        [1,4,7],
+        [2,5,8],
+        [3,6,9],
+        [1,5,9],
+        [3,5,7]
+    ]
     
-    for(let i = 0 ; i< inputs.length; i+=3){
-        if(inputs[i].value == inputs[i+1] == inputs[i+1]){
-            return inputs[i].value;
+    let isEqual = false;
+
+    //since we cannot have class name start with digit , so we have class1 to 1 during filter
+    winningCombos = winningCombos.filter(combo => combo.includes(Number(input.class.substring(5,6))));
+
+
+    //I am not using forEach because it does not support break;
+
+    for(let i = 0 ; i < winningCombos.length; i++){
+        isEqual = true;
+        let item = winningCombos[i];
+        for(let j = 0 ; j < item.length; j++){
+            let div = document.querySelector(`.class${item[j]}`)
+            let content = div.textContent;
+            if(!content || content != input.value){
+                isEqual = false;
+                break;
+            }
+        }
+
+        if(isEqual){
+            break;
         }
     }
+  
+    return isEqual;
     
 }
 
